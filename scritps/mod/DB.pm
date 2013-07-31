a37c80e910641974fb57d7ec3fb83009cf957cb2
prasunind <firefoxx@gmail.com>
2013-07-31 15:01:32 +0530

    32

--- scritps/mod/DB.pm
+++ scritps/mod/DB.pm
@@ -1,18 +1,17 @@
 package mod::DB;
-#use strict;
+use strict;
 use DBI;
 
 use Exporter; 
-@ISA = qw(Exporter);
-@EXPORT = qw(&hostdetails);
+our @ISA = qw(Exporter);
+our @EXPORT = qw(&hostdetails);
 
 
 
-
+my $dbc = DBI->connect('DBI:mysql:database=saasops;host=127.0.0.1;port=3306' ,'root', '') or die  "$DBI::errstr \n";
 
 
 sub hostdetails{
-	my $dbc = DBI->connect('DBI:mysql:database=saasops;host=127.0.0.1;port=3306' ,'root', '') or die  "$DBI::errstr \n";
 	my $servername = $_[0];
 	my $con1 = $dbc->prepare("SELECT * FROM server WHERE Hostname='$servername'")  or die "Couldn't prepare statement: $dbc->errstr";
 	$con1->execute()  or die "Couldn't execute  statement: " . $dbc->errstr; 
